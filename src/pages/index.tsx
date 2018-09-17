2ada13b75aef9b8e7d2258aad70b415c94845b7e
Chris LoPresto <chris@chrislopresto.com>
2018-09-16 23:26:54 -0400

    Add theme via styled-components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
 import * as React from 'react'
+import { theme } from '../theme/theme';
+import { createGlobalStyle, ThemeProvider } from '../theme/styled-components';
 
-export default () => <div>Hello world!</div>
+const GlobalStyle = createGlobalStyle`
+  body {
+    color: ${theme.colors.accent};
+    font-family: ${theme.fonts.mono};
+  }
+`;
+
+export default () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <h1>Hello.</h1>
+        <p>My name is Chris LoPresto.</p>
+        <GlobalStyle />
+      </>
+    </ThemeProvider>
+  )
+};
