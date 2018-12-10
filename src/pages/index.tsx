d3d11ee1be85b70bffd0aabdc36d6308555116ed
Chris LoPresto <chris@chrislopresto.com>
2018-12-10 00:01:35 -0500

    feat: Add Tailwind CSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
 import { theme } from '../theme/theme';
+import styled from '../theme/styled-components';
 import { createGlobalStyle, ThemeProvider } from '../theme/styled-components';
 import { Heading, Text } from 'rebass';
-import 'sanitize.css';
+import '../index.css';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -11,12 +12,20 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const OtherThing = styled.div`
+  color: ${theme.colors.accent};
+`;
+
 export default () => {
   return (
     <ThemeProvider theme={theme}>
       <>
-        <Heading as='h1' color='accent'>Hello</Heading>
-        <p>My name is Chris LoPresto.</p>
+        <Heading as='h1' color='accent' mb={2}>Hello</Heading>
+        <p className='mb-4'>My name is Chris LoPresto.</p>
+        <OtherThing>I am OtherThing</OtherThing>
+        <OtherThing as='h3'>I am OtherThing</OtherThing>
+        <Text color={['primary', 'accent']}>I have colors from Rebass</Text>
+        <p className='Thing'>I have colors from Tailwind</p>
         <p>I'm an <Text as='span' color='accent'>engineering leader</Text> and a <Text as='span' color='accent'>musician</Text>.</p>
         <GlobalStyle />
       </>
