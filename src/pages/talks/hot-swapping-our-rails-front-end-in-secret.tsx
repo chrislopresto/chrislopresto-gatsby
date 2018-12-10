41679c6167d10f388def1563cd249f04f328016e
Chris LoPresto <chris@chrislopresto.com>
2018-12-10 01:12:46 -0500

    Add talks in iframes

---
+++ src/pages/talks/hot-swapping-our-rails-front-end-in-secret.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import RevealJsSlideDeck from '../../components/RevealJsSlideDeck';
+
+export default () => {
+  return (
+    <RevealJsSlideDeck slug='hot-swapping-our-rails-front-end-in-secret' />
+  );
+};
---
+++ src/pages/talks/living-style-guide-driven-development.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import RevealJsSlideDeck from '../../components/RevealJsSlideDeck';
+
+export default () => {
+  return (
+    <RevealJsSlideDeck slug='living-style-guide-driven-development' />
+  );
+};
