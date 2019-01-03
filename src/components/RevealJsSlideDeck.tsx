4abe0b690a0bb85ab3f5e71a7557958424c1a056
Chris LoPresto <chris@chrislopresto.com>
2019-01-02 22:42:03 -0500

    Use S3-hosted presentations

--- src/components/RevealJsSlideDeck.tsx
+++ src/components/RevealJsSlideDeck.tsx
@@ -1,20 +1,29 @@
 import * as React from 'react';
 import styled from '../theme/styled-components';
 
+const DEFAULT_HEIGHT = '100vh';
+const DEFAULT_WIDTH = '100vw';
+
 interface Props {
-  slug: 'living-style-guide-driven-development' | 'hot-swapping-our-rails-front-end-in-secret';
+  height?: string;
+  width?: string;
+  showControls?: boolean;
+  slug: 'living-style-guide-driven-development' | 'betterment-rebranding-bonanza';
 }
 
 const Deck = styled.iframe`
   display: block;
   border: none;
-  height: 100vh;
-  width: 100vw;
+  height: ${props => props.height || DEFAULT_HEIGHT};
+  width: ${props => props.width || DEFAULT_WIDTH};
 `;
 
-const RevealJsSlideDeck: React.SFC = ({ slug }: Props) => {
-  let src = `https://chrislopresto-rails.herokuapp.com/presentations/${slug}?ph=900&pw=1600`
-  return <Deck src={src} />;
+const RevealJsSlideDeck: React.SFC<Props> = (props) => {
+  let width = props.width || DEFAULT_WIDTH;
+  let height = props.height || DEFAULT_HEIGHT;
+  let showControls = props.showControls === false ? 'false' : 'true';
+  let src = `https://s3-us-west-2.amazonaws.com/${props.slug}-index/index.html#/?ph=${height}&pw=${width}&c=${showControls}`
+  return <Deck src={src} width={width} height={height} />;
 }
 
 export default RevealJsSlideDeck;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
 import { theme } from '../theme/theme';
-import styled from '../theme/styled-components';
 import { Link } from "gatsby"
 import { createGlobalStyle, ThemeProvider } from '../theme/styled-components';
-import { Heading, Text } from 'rebass';
+import { Text, Box } from 'rebass';
 import '../index.css';
+import RevealJsSlideDeck from '../components/RevealJsSlideDeck';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -13,30 +13,31 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const OtherThing = styled.div`
-  color: ${theme.colors.accent};
-`;
-
 export default () => {
   return (
     <ThemeProvider theme={theme}>
-      <>
-        <h1 className='mb-6'>Talks</h1>
-        <ul className='mb-4'>
-          <li><Link to='/talks/hot-swapping-our-rails-front-end-in-secret'>Hot Swapping Our Rails Front End in Secret</Link></li>
-          <li><Link to='/talks/living-style-guide-driven-development'>Living Style Guide Driven Development</Link></li>
-        </ul>
+      <Box m={2}>
+        <h1 className="mb-2">Hello</h1>
+        <p className='mb-2'>My name is Chris LoPresto.</p>
+        <p className="mb-8">I'm an engineering leader and a musician.</p>
 
-        <h1 className='mb-6'>Other stuff</h1>
-        <Heading as='h1' color='accent' mb={2}>Hello</Heading>
-        <p className='mb-4'>My name is Chris LoPresto.</p>
-        <OtherThing>I am OtherThing</OtherThing>
-        <OtherThing as='h3'>I am OtherThing</OtherThing>
-        <Text color={['primary', 'accent']}>I have colors from Rebass</Text>
-        <p className='Thing'>I have colors from Tailwind</p>
-        <p>I'm an <Text as='span' color='accent'>engineering leader</Text> and a <Text as='span' color='accent'>musician</Text>.</p>
+        <h2 className='mb-2'>Talks</h2>
+        <p className='mb-2'>Here are some conference talks I have given.</p>
+        <p className="mb-4">
+          <Link to='/talks/hot-swapping-our-rails-front-end-in-secret' target='_blank'>
+            <Text mb={1}>Hot Swapping Our Rails Front End in Secret</Text>
+            <RevealJsSlideDeck slug='betterment-rebranding-bonanza' width='300px' height='169px' showControls={false} />
+          </Link>
+        </p>
+        <p className="mb-4">
+          <Link to='/talks/living-style-guide-driven-development'>
+            <Text mb={1}>Living Style Guide Driven Development</Text>
+            <RevealJsSlideDeck slug='living-style-guide-driven-development' width='300px' height='169px' showControls={false} />
+          </Link>
+        </p>
+
         <GlobalStyle />
-      </>
+      </Box>
     </ThemeProvider>
   )
 };
