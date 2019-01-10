baee162b982b1453c65f95a1faaed7c43f2bb6a5
Chris LoPresto <chris@chrislopresto.com>
2019-01-10 01:08:21 -0500

    feat: Add site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  plugins: ['gatsby-plugin-postcss', 'gatsby-plugin-styled-components', 'gatsby-plugin-typescript']
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-react-helmet'
+  ],
+  siteMetadata: {
+    title: 'Hello',
+    titleTemplate: '%s · Chris LoPresto',
+    description: 'Chris LoPresto - Engineering leader. Musician.',
+    url: 'https://chrislopresto.com',
+    image: '/images/chris-lopresto-640.jpg',
+    twitterUsername: '@chrislopresto'
+  }
 };
