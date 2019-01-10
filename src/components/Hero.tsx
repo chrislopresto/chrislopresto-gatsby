b1be43c9d2bc7b7307100d1cf1d0c4e0957b781d
Chris LoPresto <chris@chrislopresto.com>
2019-01-10 00:54:02 -0500

    feat: Provide webfont fallback

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,12 @@
 import styled from '../theme/styled-components';
 import { theme } from '../theme/theme';
+import { withFonts } from 'react-with-async-fonts';
 
-export default styled.h1`
-  font-family: ${theme.fonts.hero};
+const Hero = styled.h1`
+  font-family: ${props => (props.fonts.hero ? theme.fonts.hero : theme.fonts.sans)};
   font-size: ${theme.fontSizes[8]}px;
+  font-weight: bold;
+  text-transform: uppercase;
 `;
+
+export default withFonts(Hero);
