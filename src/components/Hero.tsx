9725ec41c93f18a590f3f824787a6a1e8bf6d5f5
Chris LoPresto <chris@chrislopresto.com>
2019-01-24 23:59:37 -0500

    feat: Wire up site mode context

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,22 @@
+import * as React from 'react';
 import styled from '../theme/styled-components';
 import { withFonts } from 'react-with-async-fonts';
+import { SiteMode, SiteModeContextConsumer, withSiteModeContext } from '../theme/SiteModeContext';
 
-const Hero = styled.h1`
+interface Props {
+  className: string;
+  siteModeContext: SiteMode;
+}
+
+const Hero: React.FC<Props> = ({ className, siteModeContext, children }) => {
+  return (
+    <h1 className={className} onClick={siteModeContext.setTheme}>
+      {children}
+    </h1>
+  );
+};
+
+const StyledHero = styled(Hero)`
   font-family: ${props => (props.theme.fonts.hero ? props.theme.fonts.hero : props.theme.fonts.sans)};
   font-size: ${props => props.theme.fontSizes[8]}px;
   font-weight: bold;
@@ -9,4 +24,4 @@ const Hero = styled.h1`
   color: ${props => props.theme.colors.accent};
 `;
 
-export default withFonts(Hero);
+export default withFonts(withSiteModeContext(StyledHero));
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as fonts from '../fonts';
 import { createGlobalStyle, ThemeProvider } from '../theme/styled-components';
 import { darkTheme, lightTheme } from '../theme/theme';
+import { SiteModeContextConsumer, SiteModeContextProvider, SiteMode } from '../theme/SiteModeContext';
 import SEO from './SEO';
 
 const GlobalStyle = createGlobalStyle`
@@ -30,16 +31,51 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const Layout: React.FunctionComponent = ({ children }) => {
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <>
-        <SEO />
-        {children}
-        <GlobalStyle />
-      </>
-    </ThemeProvider>
-  );
-};
+interface State {
+  theme: 'light' | 'dark';
+  setTheme: () => void;
+}
 
-export default Layout;
+export default class Layout extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      theme: 'dark',
+      setTheme: this.setTheme
+    };
+  }
+
+  setTheme = () => {
+    this.setState({
+      theme: this.state.theme === 'light' ? 'dark' : 'light'
+    });
+  };
+
+  render() {
+    const defaultSiteMode: SiteMode = {
+      theme: 'dark',
+      setTheme: this.setTheme
+    };
+
+    return (
+      <SiteModeContextProvider value={this.state}>
+        <SiteModeContextConsumer>
+          {siteModeContext => {
+            let theme = siteModeContext!.theme === 'light' ? lightTheme : darkTheme;
+
+            return (
+              <ThemeProvider theme={theme}>
+                <>
+                  <SEO />
+                  {this.props.children}
+                  <GlobalStyle />
+                </>
+              </ThemeProvider>
+            );
+          }}
+        </SiteModeContextConsumer>
+      </SiteModeContextProvider>
+    );
+  }
+}
