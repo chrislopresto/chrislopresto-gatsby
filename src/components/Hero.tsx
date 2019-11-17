a97b81779ae004398a663a9d31a87544b27e39cc
Chris LoPresto <chris@chrislopresto.com>
2019-11-16 21:11:19 -0500

    dev: Fix FontSubscriber / FontObserver usage

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,29 +1,35 @@
 import * as React from 'react';
-import styled from '../theme/styled-components';
-import { withFonts } from 'react-with-async-fonts';
+import { FontSubscriber } from 'react-with-async-fonts';
 import { useSiteMode } from '../theme/SiteModeContext';
+import styled from '../theme/styled-components';
 
 interface Props {
   className: string;
+  children: React.ReactNode;
 }
 
-const Hero: React.FC<Props> = ({ className, children }) => {
-  const [, dispatch] = useSiteMode();
-  const toggleMode = () => dispatch({ type: 'toggleMode' });
-
-  return (
-    <h1 className={className} onClick={toggleMode}>
-      {children}
-    </h1>
-  );
-};
-
-const StyledHero = styled(Hero)`
+const StyledHeader = styled.h1`
   font-family: ${props => (props.theme.fonts.hero ? props.theme.fonts.hero : props.theme.fonts.sans)};
   font-size: ${props => props.theme.fontSizes[8]}px;
   font-weight: bold;
   text-transform: uppercase;
   color: ${props => props.theme.colors.accent};
+  cursor: pointer;
 `;
 
-export default withFonts(StyledHero);
+const Hero = ({ className, children }: Props) => {
+  const [, dispatch] = useSiteMode();
+  const toggleMode = () => dispatch({ type: 'toggleMode' });
+
+  return (
+    <FontSubscriber>
+      {() => (
+        <StyledHeader className={className} onClick={toggleMode}>
+          {children}
+        </StyledHeader>
+      )}
+    </FontSubscriber>
+  );
+};
+
+export default Hero;
