fd1b36c2c5a9ad94665aa351563ebf82d0bf2dc9
Chris LoPresto <chris@chrislopresto.com>
2019-11-17 18:09:20 -0500

    refactor: Move FontObserver into shared Layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
 import { Link } from 'gatsby';
-import { Text, Box } from 'rebass';
-import '../index.css';
-import RevealJsSlideDeck from '../components/RevealJsSlideDeck';
-import Layout from '../components/Layout';
-import Hero from '../components/Hero';
-import { FontObserver } from 'react-with-async-fonts';
+import React from 'react';
+import { Box, Text } from 'rebass';
 import styled from 'styled-components';
+import Hero from '../components/Hero';
+import Layout from '../components/Layout';
+import RevealJsSlideDeck from '../components/RevealJsSlideDeck';
+import '../index.css';
 
 const LowCap = styled.span`
   font-size: ${props => props.theme.fontSizes[8] * 0.68}px;
@@ -14,42 +13,34 @@ const LowCap = styled.span`
 
 export default () => {
   return (
-    // @ts-ignore incorrect FontObserver children types
-    <FontObserver hero="Marvin Visions Big">
-      <Layout>
-        <Box m={2}>
-          <Hero className="mb-2">
-            Chris L<LowCap>o</LowCap>Presto
-          </Hero>
-          <p className="mb-2">My name is Chris LoPresto.</p>
-          <p className="mb-8">I'm an engineering leader and a musician.</p>
+    <Layout>
+      <Box m={2}>
+        <Hero className="mb-2">
+          Chris L<LowCap>o</LowCap>Presto
+        </Hero>
+        <p className="mb-2">My name is Chris LoPresto.</p>
+        <p className="mb-8">I'm an engineering leader and a musician.</p>
 
-          <h2 className="mb-2">Talks</h2>
-          <p className="mb-2">Here are some conference talks I have given.</p>
-          <div className="mb-4">
-            <Link to="/presentations/hot-swapping-our-rails-front-end-in-secret" target="_blank">
-              <Text mb={1}>Hot Swapping Our Rails Front End in Secret</Text>
-              <RevealJsSlideDeck
-                slug="betterment-rebranding-bonanza"
-                width="300px"
-                height="169px"
-                showControls={false}
-              />
-            </Link>
-          </div>
-          <div className="mb-4">
-            <Link to="/presentations/living-style-guide-driven-development">
-              <Text mb={1}>Living Style Guide Driven Development</Text>
-              <RevealJsSlideDeck
-                slug="living-style-guide-driven-development"
-                width="300px"
-                height="169px"
-                showControls={false}
-              />
-            </Link>
-          </div>
-        </Box>
-      </Layout>
-    </FontObserver>
+        <h2 className="mb-2">Talks</h2>
+        <p className="mb-2">Here are some conference talks I have given.</p>
+        <div className="mb-4">
+          <Link to="/presentations/hot-swapping-our-rails-front-end-in-secret" target="_blank">
+            <Text mb={1}>Hot Swapping Our Rails Front End in Secret</Text>
+            <RevealJsSlideDeck slug="betterment-rebranding-bonanza" width="300px" height="169px" showControls={false} />
+          </Link>
+        </div>
+        <div className="mb-4">
+          <Link to="/presentations/living-style-guide-driven-development">
+            <Text mb={1}>Living Style Guide Driven Development</Text>
+            <RevealJsSlideDeck
+              slug="living-style-guide-driven-development"
+              width="300px"
+              height="169px"
+              showControls={false}
+            />
+          </Link>
+        </div>
+      </Box>
+    </Layout>
   );
 };
