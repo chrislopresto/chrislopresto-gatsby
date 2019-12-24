f6325882b00ed0d19dae168a08966afe10dc2807
Chris LoPresto <chris@chrislopresto.com>
2019-12-24 17:12:36 -0600

    feat: Toggle site mode class on body element

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,26 +1,19 @@
 import React, { ReactNode } from 'react';
 import { FontObserver } from 'react-with-async-fonts';
-import { SiteModeProvider, useSiteModeState } from '../theme/SiteModeContext';
+import { SiteModeProvider } from '../theme/SiteModeContext';
 import SEO from './SEO';
-
-const Content = ({ children }: { children: ReactNode }) => {
-  const siteModeState = useSiteModeState();
-
-  return (
-    // @ts-ignore incorrect FontObserver children types
-    <FontObserver hero="Marvin Visions Big">
-      <div className={`mode-${siteModeState.mode}`}>
-        <SEO />
-        {children}
-      </div>
-    </FontObserver>
-  );
-};
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <SiteModeProvider mode="dark">
-      <Content>{children}</Content>
+      {/*
+        // @ts-ignore incorrect FontObserver children types */}
+      <FontObserver hero="Marvin Visions Big">
+        <>
+          <SEO />
+          {children}
+        </>
+      </FontObserver>
     </SiteModeProvider>
   );
 };
