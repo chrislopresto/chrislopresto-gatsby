5326d90d2bd2ede5b03c0002f10e3fa2f7081fb5
Chris LoPresto <chris@chrislopresto.com>
2020-01-01 21:06:32 -0500

    style: Tweak home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,22 +20,24 @@ export default () => {
         </section>
 
         <section className="mb-10 max-w-3xl">
-          <h2 className="mb-2">Bio</h2>
-          <p className="mb-2">
+          <h2 className="mb-2 leading-none">Bio</h2>
+          <p className="mb-5">
             Chris LoPresto grew up as an oldest child in Delaware, the oldest state. He picked up degrees in Jazz
             Performance (piano) and Computer Science at Exit 9 of the NJ Turnpike before continuing on up to New York
             City.
           </p>
-          <p className="mb-2">
+          <p className="mb-5">
             Chris has performed at iconic venues<sup className="text-xs">*</sup> and dive bars
-            <sup className="text-xs">**</sup> around the world<sup className="text-xs">***</sup>, most notably with Roc
-            Nation / Epic Records recording artist Hugo. Chris has performed on Late Night with David Letterman, Jimmy
-            Kimmel Live!, X Factor, and morning shows that film far too early.
+            <sup className="text-xs">**</sup> around the world
+            <sup className="text-xs">***</sup>, most notably with Roc Nation / Epic Records recording artist Hugo. Chris
+            has performed on Late Night with David Letterman, Jimmy Kimmel Live!, X Factor, and morning shows that film
+            far too early.
           </p>
-          <p className="mb-2">
+          <p className="mb-5">
             More miscellaneously, Chris is an Eagle Scout who has visited 46 of the 50 states and ridden a private
-            elevator with a pope<sup className="text-xs">****</sup>. He lives in New York City with his wife, Louise,
-            and his dog, Roland, and is always game to debate which is better, cake or pie
+            elevator with a pope
+            <sup className="text-xs">****</sup>. He lives in New York City with his wife, Louise, and his dog, Roland,
+            and is always game to debate which is better, cake or pie
             <sup className="text-xs">*****</sup>.
           </p>
           <p className="mb-2 text-sm">
@@ -56,7 +58,7 @@ export default () => {
         </section>
 
         <section className="mb-10">
-          <h2 className="mb-2">Talks</h2>
+          <h2 className="mb-2 leading-none">Talks</h2>
           <p className="mb-2">Here are some conference talks I have given.</p>
           <div className="mb-6">
             <Link to="/presentations/hot-swapping-our-rails-front-end-in-secret" target="_blank">
