0a25da0e25e358fcfeabc3eaa024f29a69367b1d
Chris LoPresto <chris@chrislopresto.com>
2020-03-26 22:03:27 -0400

    feat: Remove Hero component

--- src/components/hero/hero.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Hero } from '.';
-
-export default { title: 'Hero' };
-
-export const example = () => <Hero />;
-
-export const styled = () => <Hero className="p-4 border-dashed border-teal-400 border-4" />;
