5eeea3429444735bceb34d4f86eae3d852fef183
Chris LoPresto <chris@chrislopresto.com>
2020-04-05 18:09:35 -0400

    fix: Fix resume page styles

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,6 +1,14 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
+import { ChromelessLayout } from '../layouts/chromeless-layout';
 
 export default () => (
-  <iframe title="Chris LoPresto Resume" sx={{ width: '100%', height: '100vh' }} src="../../chris-lopresto-resume.pdf" />
+  <ChromelessLayout>
+    <iframe
+      frameBorder={0}
+      title="Chris LoPresto Resume"
+      sx={{ width: '100%', height: '100vh' }}
+      src="../../chris-lopresto-resume.pdf"
+    />
+  </ChromelessLayout>
 );
